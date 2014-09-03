a8b41995519099054f31efb05db0ddb4609d2524
mawulorm <fra218@lehigh.edu>
2014-09-03 00:42:59 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//define the class
+public class WelcomeClass{
+//   add main method
+   public static void main(String [ ] args) {
+       
+//  print the statement with box around it
+    System.out.println("-----------------");
+    System.out.println("  | WELCOME |");
+    System.out.println("-----------------");
+    System.out.println("^  ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\ / \\ / \\ / \\ / \\ / \\");
+    System.out.println("|-F --R --A --2 --1--8-|");
+    System.out.println("I am originally from Ghana in West Africa");
+    System.out.println("test");
+    
+    //test/testtest
+   }
+}
